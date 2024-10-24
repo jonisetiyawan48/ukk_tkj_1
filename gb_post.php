80adb6a9f2881cfa72acdfcdeb43411f16087156
Joni Setiyawan Saputra <75488331+jonisetiyawan48@users.noreply.github.com>
2024-10-25 00:52:29 +0700

    Update gb_post.php

--- gb_post.php
+++ gb_post.php
@@ -5,12 +5,11 @@ session_start();
 include 'koneksi.php';
 
 $nama = $_POST['nama'];
-$pesan = $_POST['pesan'];
+$pesan = htmlspecialchars($_POST['pesan']);
 $nama = mysqli_real_escape_string($conn, $nama);
 
 $nama = htmlspecialchars($nama, ENT_QUOTES, 'UTF-8');
-$pesan = htmlspecialchars($pesan, ENT_QUOTES, 'UTF-8');
-$insert = mysqli_query($conn, "INSERT INTO guestbook (id, tanggal, nama, pesan) VALUES(NULL, NOW(), '{$nama}', '{$pesan}')");
+$insert = mysqli_query($conn, "INSERT INTO guestbook (id, tanggal, nama, pesan) VALUES(NULL, NOW(), '$nama', '$pesan')");
 
 if ($insert) {
 	echo $insert;
@@ -18,5 +17,4 @@ if ($insert) {
 
 
 //tambahkan atau perbaiki script diantara dibaris 7 sampai  15
-//sql code injection  di guestbook bagian pesan   '); CREATE TABLE hacked (id INT); --
-
+//sql code injection  di guestbook bagian pesan   joni') UNION SELECT NULL,now(),'joni','joni'; -- '
